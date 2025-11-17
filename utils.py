ceee5ff0b130c806ff685d264dbbab0c142afb49
Andre <andrechu17@gmail.com>
2025-11-17 22:30:32 +0800

    Delete MCO1 Files
    These files are already inside the MCO1_Previous branch.

--- utils.py
+++
@@ -1,9 +0,0 @@
-#Python file for utility/helper functions
-from IPython.display import HTML
-
-def display_results(dataframe):
-    return HTML(f"""
-        <div style="height:400px; overflow-y:auto; border:1px; padding:10px; background-color:clear;">
-        {dataframe.to_html(index=False)}
-        </div>
-        """)
