69773cb0ae8329f66f7d355ebfd6bc9a03f40734
Angelo Rocha <angelorocha0930@gmail.com>
2025-11-30 08:16:54 +0800

    test

--- server.js
+++ server.js
@@ -340,29 +340,7 @@ app.post("/tx/commit", async (req, res) => {
 
         log(`COMMIT tx=${txId}`);
 
-        // Fetch genre information
-        try {
-            const res = await fetch(url + "/tx/read", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ txId, title_id: titleId })
-            });
-
-            const json = await res.json();
-
-            //if row exists show rating and title info
-            if (json.ok && json.row) {
-                const rating = json.row.average_rating;
-                appendLog(`VALUE tx=${txId} title=${titleId} rating=${rating}`);
-            }
-
-            console.log(json)
-
-            appendLog(json);
-
-        } catch (err) {
-            appendLog({ ok: false, error: err.message });
-        }
+        conole
 
         await replicateTransaction(commitEntry); // change this later
 
